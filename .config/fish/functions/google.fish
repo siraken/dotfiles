096d1d1c39553998826777228d0ee382f9bb553c
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-06-16 17:25:32 +0900

    chore(fish): remove 2 functions

--- .config/fish/functions/google.fish
+++
@@ -1,6 +0,0 @@
-# Google Search from Fish
-
-function google
-  set query (string join "+" $argv)
-  open "https://www.google.com/search?q="$query
-end
--- .config/fish/functions/meme.fish
+++
@@ -1,6 +0,0 @@
-# Know Your Meme Search from Fish
-
-function meme
-  set query (string join "+" $argv)
-  open "https://knowyourmeme.com/search?q="$query
-end
