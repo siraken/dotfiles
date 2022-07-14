7823b9520fe8676fe5924bac0914ec0d94f8acd0
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-07-14 19:09:36 +0900

    refactor: posh config

--- .config/Microsoft.PowerShell_profile.ps1
+++ .config/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,18 @@
-# oh-my-posh init pwsh | Invoke-Expression
-Invoke-Expression (&starship init powershell)
+#
+# PowerShell Configuration
+#
 
+# Initialize with Starship or Oh My Posh
+# Invoke-Expression (&starship init powershell)
+oh-my-posh init pwsh | Invoke-Expression
+
+# --- Aliases
 Set-Alias wget Invoke-WebRequest
 Set-Alias which Get-Command
+
+Set-Alias Chrome "C:\Program Files\Google\Chrome\Application\chrome.exe"
+Set-Alias Brave "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
 
 Set-Alias vim nvim
 Set-Alias vi nvim
 
-Set-Alias Chrome "C:\Program Files\Google\Chrome\Application\chrome.exe"
-Set-Alias Brave "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
