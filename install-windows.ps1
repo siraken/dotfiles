b41504c70726eac3298b6340bfef12b68afd6619
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-07-14 17:55:27 +0900

    feat: windows symlink

--- install-windows.ps1
+++ install-windows.ps1
@@ -1,2 +1,8 @@
+# Starship
+New-Item -Force -ItemType SymbolicLink -Value $env:USERPROFILE\dotfiles\.config\starship.toml -Path $env:USERPROFILE\.config\starship.toml
+
 # NeoVim
 New-Item -Force -ItemType SymbolicLink -Value $env:USERPROFILE\dotfiles\.config\nvim\ -Path $env:USERPROFILE\AppData\Local\nvim
+
+# Hyper
+New-Item -Force -ItemType SymbolicLink -Value $env:USERPROFILE\dotfiles\.hyper.js -Path $env:USERPROFILE\AppData\Roaming\Hyper\.hyper.js
