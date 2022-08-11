f4cd4ceccfa7e17787d56af86d97d61e70b87402
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-08-12 00:53:39 +0900

    chore: some change

--- .config/nvim/after/plugin/lspconfig.rc.lua
+++ .config/nvim/after/plugin/lspconfig.rc.lua
@@ -1,16 +1,27 @@
--- local status, nvim_lsp = pcall(require, 'lspconfig')
--- if (not status) then return end
+local status, nvim_lsp = pcall(require, 'lspconfig')
+if (not status) then return end
 
--- local protocol = require('vim.lsp.protocol')
+local api = vim.api
+local protocol = require('vim.lsp.protocol')
 
--- local on_attach = function(client, bufnr)
---   if client.server_capabilities.documentFormattingProvider then
---     vim.api.nvim_command [[augroup Format]]
---     vim.api.nvim_command [[autocmd! * <buffer>]]
---     vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
---     vim.api.nvim_command [[augroup END]]
---   end
--- end
+local on_attach = function(client, bufnr)
+  -- if client.server_capabilities.documentFormattingProvider then
+  --   api.nvim_command [[augroup Format]]
+  --   api.nvim_command [[autocmd! * <buffer>]]
+  --   api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+  --   api.nvim_command [[augroup END]]
+  -- end
+
+  -- ↓ Previous config
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  local opts = { noremap = true, silent = true }
+
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+
+  require 'completion'.on_attach(client, bufnr)
+end
 
 -- nvim_lsp.tsserver.setup {
 --   on_attach = on_attach,
