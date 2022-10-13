a2b6a6c30b363634e8b34d13b4d98bce094a0b30
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-10-13 23:18:59 +0900

    chore(nvim): lspsaga

--- .config/nvim/after/plugin/lspsaga.rc.lua
+++ .config/nvim/after/plugin/lspsaga.rc.lua
@@ -5,11 +5,10 @@ local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 saga.init_lsp_saga {
-  server_filetype_map = {}
 }
 
-keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<cr>', opts)
 keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<cr>', opts)
+keymap.set('n', 'gn', '<Cmd>Lspsaga rename<cr>', opts)
+keymap.set('n', 'gl', '<Cmd>Lspsaga show_line_diagnostics<cr>', opts)
+keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<cr>', opts)
 keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<cr>', opts)
-keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<cr>', opts)
-keymap.set('i', 'gp', '<Cmd>Lspsaga preview_definition<cr>', opts)
