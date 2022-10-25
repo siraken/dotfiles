4714732f89d412d471b508ab13e39dc4b968561c
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-10-25 10:43:52 +0900

    chore: use native vim

--- .config/Microsoft.PowerShell_profile.ps1
+++ .config/Microsoft.PowerShell_profile.ps1
@@ -15,7 +15,7 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\$env:POSH_MY_THEME.omp.json
 
 # --- Functions
 function Get-GitStatus {
-  & git status $args
+    & git status $args
 }
 
 # --- Aliases
@@ -25,8 +25,5 @@ Set-Alias which Get-Command
 Set-Alias Chrome "C:\Program Files\Google\Chrome\Application\chrome.exe"
 Set-Alias Brave "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
 
-Set-Alias vim nvim
-Set-Alias vi nvim
 Set-Alias g git
 Set-Alias gs Get-GitStatus
-
