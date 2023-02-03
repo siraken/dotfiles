f6602c8453c319bf4e975a10a1ff3199aad13403
Kento Shirasawa <shirasawa.kento@gmail.com>
2023-02-03 12:22:01 +0900

    feat(fish): git-add-url

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,32 +14,6 @@ alias md="mkdir"
 alias e="emacs"
 
 ##################################################
-# ==> Functions
-##################################################
-# Docker upload
-function docker-upload
-  echo $1
-  # docker build -t $1 .
-  # docker save -o $1.tar $1
-end
-
-# Weather
-function weather
-  curl wttr.in/$argv
-end
-
-# Initialize Go app
-function go-init
-  echo "Enter the package name:" && read package;
-  mkdir -p $package && cd $package
-  go mod init github.com/$dirname/$package
-  git init && touch main.go README.md
-end
-
-# source (dirname (status --current-filename))/f.fish
-# source (dirname (status --current-filename))/fisher-install.fish
-
-##################################################
 # ==> Path
 ##################################################
 status --is-interactive; and rbenv init - fish | source
@@ -69,3 +43,41 @@ set -x GH_URL https://github.com
 set --universal nvm_default_version v16.19.0
 # set --universal nvm_default_version v18.12.1
 set --universal nvm_default_packages yarn
+
+##################################################
+# ==> Functions
+##################################################
+# Docker upload
+function docker-upload
+  echo $1
+  # docker build -t $1 .
+  # docker save -o $1.tar $1
+end
+
+# Weather
+function weather
+  curl wttr.in/$argv
+end
+
+# Initialize Go app
+function go-init
+  echo "Enter the package name:" && read package;
+  mkdir -p $package && cd $package
+  go mod init github.com/$dirname/$package
+  git init && touch main.go README.md
+end
+
+# Add git remote URL with current directory name
+function git-add-url
+  echo "Enter the username:" && read username;
+  if test -n "$username"
+    git remote add origin "$GH_URL/$username/$(basename $(pwd)).git"
+    git remote -v
+  else
+    echo "Please provide the username you want to use."
+  end
+end
+
+# source (dirname (status --current-filename))/f.fish
+# source (dirname (status --current-filename))/fisher-install.fish
+
