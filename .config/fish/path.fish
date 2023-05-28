c32e1612b64beeb2d578b6cba53fa9b06525dbcb
Kento Shirasawa <shirasawa.kento@gmail.com>
2023-05-28 09:24:16 +0900

    fix(fish): sort path loading order

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -3,33 +3,29 @@
 ##################################################
 set -gx EDITOR nvim
 
-set -g GOPATH $HOME/go
-set -g JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx GOPATH $HOME/go
 
 set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH /opt/homebrew/sbin $PATH
-
-# Use homebrew-installed openssl
-set -gx PATH "/opt/homebrew/opt/openssl@3/bin" $PATH
-
-set -gx VOLTA_HOME "$HOME/.volta"
-
+set -gx PATH /opt/homebrew/opt/openssl@3/bin $PATH # Use openssl installed by homebrew
 set -gx PATH bin $PATH
 set -gx PATH "$GOPATH/bin" $PATH
-set -gx PATH "$HOME/.cargo/bin" $PATH
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+set -gx PATH "$HOME/.cargo/bin" $PATH
+set -gx PATH "$HOME/.deno/bin" $PATH
 set -gx PATH "$HOME/.composer/vendor/bin" $PATH
 set -gx PATH "$HOME/Developer/flutter/bin" $PATH
 set -gx PATH "$HOME/bin" $PATH
 set -gx PATH "$HOME/.local/bin" $PATH
 set -gx PATH "$HOME/.progate/bin" $PATH
-
-# TODO: probably need to be added if statement
 set -gx PATH "$HOME/Library/Android/sdk/platform-tools" $PATH
-set -gx PATH "$HOME/.deno/bin" $PATH
 set -gx PATH "$HOME/Library/Application Support/JetBrains/Toolbox/scripts" $PATH
 
 test -e "$HOME/.phpbrew/phpbrew.fish"; and source "$HOME/.phpbrew/phpbrew.fish"
+# FIXME: rye does not work on fish shell
+# test -e "$HOME/.rye/env"; and source "$HOME/.rye/env"
 
 command -q rbenv; and status --is-interactive; and rbenv init - fish | source
 command -q pyenv; and status --is-interactive; and pyenv init - | source
