e5040d46396221166ef887902a7e78c702cfe312
Kento Shirasawa <shirasawa.kento@gmail.com>
2023-09-17 11:15:30 +0900

    fix(posh): some conf added

---
+++ .config/powershell/includes/alias.ps1
@@ -0,0 +1,13 @@
+#
+# Aliases
+#
+
+Set-Alias wget Invoke-WebRequest
+Set-Alias which Get-Command
+Set-Alias g git
+Set-Alias gs Get-GitStatus
+Set-Alias vim nvim
+# Set-Alias emacs Edit-EmacsCUI
+
+Set-Alias Chrome "C:\Program Files\Google\Chrome\Application\chrome.exe"
+Set-Alias Brave "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
