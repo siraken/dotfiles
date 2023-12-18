3122e9987c0c87849a22068a9d2c2526e120ff13
Kento Shirasawa <shirasawa.kento@gmail.com>
2023-12-18 21:07:37 +0900

    chore: update

--- .config/fish/completion.fish
+++ .config/fish/completion.fish
@@ -1,16 +1,12 @@
 # pnpm
 if test -f $FISH_CONFIG_PATH/pnpm.fish
-  source $FISH_CONFIG_PATH/pnpm.fish
-else
-  # command -q pnpm; and status --is-interactive;
-  # and pnpm install-completion | source
+    source $FISH_CONFIG_PATH/pnpm.fish
 end
 
 # gcloud
 if test -f $HOME/Developer/google-cloud-sdk/completion.fish.inc
-  source "$HOME/Developer/google-cloud-sdk/completion.fish.inc"
+    source "$HOME/Developer/google-cloud-sdk/completion.fish.inc"
 end
 
 # dotnet
 complete -f -c dotnet -a "(dotnet complete (commandline -cp))"
-
