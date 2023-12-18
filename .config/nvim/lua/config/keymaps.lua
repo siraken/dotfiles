872d38b86282a57c1436841c0f238d5a9ffb5c76
Kento Shirasawa <shirasawa.kento@gmail.com>
2023-12-18 15:54:47 +0900

    add(nvim): conf from old config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,43 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local keymap = vim.keymap
+
+-- Increment / decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Split window
+keymap.set("n", "ss", ":split<CR><C-W>w")
+keymap.set("n", "sv", ":vsplit<CR><C-w>w")
+
+-- Move window
+keymap.set("n", "<Space>", "<C-w>w")
+keymap.set("", "sh", "<C-w>h")
+keymap.set("", "sk", "<C-w>k")
+keymap.set("", "sj", "<C-w>j")
+keymap.set("", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Close buffer
+keymap.set("n", "<C-w>q", ":bd<CR>")
+
+-- Use Shift + U as redo
+keymap.set("n", "U", "<C-r>")
+
+-- Reselect visual block after indents
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
+
+-- Use Tab, Shift + Tab
+keymap.set("i", "<S-Tab>", "<C-d>")
+keymap.set("v", "<Tab>", ">>")
+keymap.set("v", "<S-Tab>", "<<")
+
+-- Stay visual mode after formatting
+keymap.set("v", "=", "=gv")
