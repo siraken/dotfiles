d5eba20e22d94f9eb14f99d29b739ad25b731e46
Kento Shirasawa <shirasawa.kento@gmail.com>
2024-01-04 21:53:38 +0900

    fix(zsh): ll and cat

--- .config/zsh/5-alias.zsh
+++ .config/zsh/5-alias.zsh
@@ -35,12 +35,12 @@ alias mamp-htdocs="cd /Applications/MAMP/htdocs"
 alias lisp-server='sbcl --load \
   $HOME/.local/share/nvim/site/pack/packer/start/vlime/lisp/start-vlime.lisp'
 
-# if type -q bat
-#   alias cat="bat"
-# end
-#
-# if type -q eza
-#   alias ll="eza -l -g --icons"
-#   alias lla="ll -a"
-# end
+if type bat > /dev/null; then
+  alias cat="bat"
+fi
+
+if type eza > /dev/null; then
+  alias ll="eza -l -g --icons"
+  alias lla="ll -a"
+fi
 
