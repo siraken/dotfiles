d2c1702f7e6a7b3d8d0eedb10df5f330be3a5522
Kento Shirasawa <shirasawa.kento@gmail.com>
2024-01-04 13:26:56 +0900

    fix(wez): first config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,6 @@
--- Pull in the wezterm API
+-- Configuration
+-- https://wezfurlong.org/wezterm/config/files.html
 local wezterm = require("wezterm")
-
--- This table will hold the configuration.
 local config = {}
 
 -- In newer versions of wezterm, use the config_builder which will
@@ -10,10 +9,15 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
+config.color_scheme = "Tokyo Night"
+config.font_size = 16
+config.font = wezterm.font_with_fallback({
+  "JetBrains Mono",
+  "Hack Nerd Font Mono",
+  "Fira Code",
+})
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.window_background_opacity = 0.95
+config.default_cwd = "$HOME/repos"
 
--- For example, changing the color scheme:
-config.color_scheme = "AdventureTime"
-
--- and finally, return the configuration to wezterm
 return config
