b153d50325c547fd9e9e75a9a4ab652883b2e64a
Kento Shirasawa <shirasawa.kento@gmail.com>
2024-01-04 23:02:51 +0900

    feat(nvim): enable tokyonight transparency

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -3,20 +3,15 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   branch = "main",
-  -- config = function()
-  --   local tokyonight = require("tokyonight")
-  --   tokyonight.setup {
-  --     style = 'night',
-  --     light_style = 'day',
-  --     transparent = false,
-  --     terminal_colors = true,
-  --     styles = {
-  --       sidebars = "transparent",
-  --       floats = "transparent",
-  --     },
-  --     day_brightness = 0.3,
-  --     lualine_bold = true,
-  --   }
-  --   vim.api.nvim_command [[colorscheme tokyonight]]
-  -- end,
+  opts = {
+    style = "night",
+    light_style = "day",
+    transparent = true,
+    terminal_colors = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+    day_brightness = 0.3,
+  },
 }
