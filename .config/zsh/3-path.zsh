f07688d35093c5e52cb4cd6f7a91c69fa7883087
Kento Shirasawa <shirasawa.kento@gmail.com>
2024-01-13 09:06:56 +0900

    fix(zsh): path order

--- .config/zsh/3-path.zsh
+++ .config/zsh/3-path.zsh
@@ -1,15 +1,17 @@
 ##################################################
 # Path
 ##################################################
+# Variables
 export SSH_AUTH_SOCK="$HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock"
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home"
 export VOLTA_HOME="$HOME/.volta"
 export GOPATH="$HOME/go"
 export PNPM_HOME="$HOME/.pnpm"
 export MODULAR_HOME="$HOME/.modular"
+
+# Path
 export PATH="/opt/homebrew/bin:$PATH"
 export PATH="/opt/homebrew/sbin:$PATH"
-export PATH="/opt/homebrew/opt/openssl@3/bin:$PATH" # Use openssl installed by homebrew
 export PATH="$GOPATH/bin:$PATH"
 export PATH="$VOLTA_HOME/bin:$PATH"
 export PATH="$PNPM_HOME:$PATH"
@@ -19,9 +21,11 @@ export PATH="$HOME/.deno/bin:$PATH"
 export PATH="$HOME/.composer/vendor/bin:$PATH"
 export PATH="$HOME/Developer/flutter/bin:$PATH"
 export PATH="$HOME/.pub-cache/bin:$PATH"
+export PATH="$HOME/Library/Android/sdk/platform-tools:$PATH"
+export PATH="$HOME/Library/Application Support/JetBrains/Toolbox/scripts:$PATH"
+export PATH="$HOME/Library/Application Support/Coursier/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.progate/bin:$PATH"
-export PATH="$HOME/Library/Android/sdk/platform-tools:$PATH"
-export PATH="$HOME/Library/Application Support/JetBrains/Toolbox/scripts:$PATH"
-
+export PATH="bin:$PATH"
+export PATH="/opt/homebrew/opt/openssl@3/bin:$PATH" # Use openssl installed by homebrew
