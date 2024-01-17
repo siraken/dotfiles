a2a326503ee751f40351a1a22c61ea0bccf2f16b
Kento Shirasawa <shirasawa.kento@gmail.com>
2024-01-17 09:23:14 +0900

    add(cargo): pagefind

--- installers/cargo.sh
+++ installers/cargo.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Install Rust if not installed
-if ! type rustc > /dev/null 2>&1; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+if ! type rustc >/dev/null 2>&1; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
 packages=(
-    "diesel_cli"
-    "dioxus-cli"
-    "du-dust"
-    "evcxr_repl"
-    "gitoxide"
+  "diesel_cli"
+  "dioxus-cli"
+  "du-dust"
+  "evcxr_repl"
+  "gitoxide"
+  "pagefind"
 )
 
 # Install packages
 printf "Installing packages..."
 
-for package in "${packages[@]}"
-do
+for package in "${packages[@]}"; do
   cargo install $package
 done
