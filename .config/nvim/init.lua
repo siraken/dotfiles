a1829cae57901503c999725570c298705b6d528f
Kento Shirasawa <shirasawa.kento@gmail.com>
2024-04-08 19:44:12 +0900

    add(nvim): neovide config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,14 +2,16 @@ require("config.lazy")
 
 local has = vim.fn.has
 
-local is_mac = has 'macunix'
-local is_win = has 'win32'
-local is_wsl = has 'wsl'
+local is_mac = has("macunix")
+local is_win = has("win32")
+local is_wsl = has("wsl")
 
 if is_mac then
-  require('config.macos')
+  require("config.macos")
 end
 
 if is_win then
-  require('config.windows')
+  require("config.windows")
 end
+
+require("config.neovide")
---
+++ .config/nvim/lua/config/neovide.lua
@@ -0,0 +1,13 @@
+local g = vim.g
+local o = vim.o
+
+local alpha = function()
+  return string.format("%x", math.floor(255 * g.transparency or 0.8))
+end
+
+if g.neovide then
+  g.neovide_transparency = 0.5
+  g.transparency = 0.8
+  g.neovide_background_color = "#0f1117" .. alpha()
+  g.neovide_window_blurred = true
+end
