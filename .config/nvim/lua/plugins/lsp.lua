4613a969e8288c0b02711cbd7eee587f96e4436b
Kento Shirasawa <shirasawa.kento@gmail.com>
2024-04-13 01:49:40 +0900

    chore(nvim): lspconfig

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,24 @@
 return {
+  -- NeoVim LSP Config
+  -- https://github.com/neovim/nvim-lspconfig
+  -- Mason Config
+  -- https://github.com/williamboman/mason.nvim
+  -- Mason LSP Config
+  -- https://github.com/williamboman/mason-lspconfig.nvim
+  -- Neovim LSP
+  {
+    "neovim/nvim-lspconfig",
+    init_options = {
+      userLanguages = {
+        eelixir = "html-eex",
+        eruby = "erb",
+        rust = "html",
+      },
+    },
+  },
   -- https://github.com/sheerun/vim-polyglot
   {
     "sheerun/vim-polyglot",
     -- polyglot.setup {}
-  }
+  },
 }
