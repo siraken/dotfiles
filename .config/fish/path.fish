a28c37e64f436a5a6a3680296a398ecd43bb1e75
Kento Shirasawa <shirasawa.kento@gmail.com>
2024-05-14 16:07:30 +0900

    fix(shell): path

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -10,26 +10,26 @@ set -gx PNPM_HOME "$HOME/.pnpm"
 set -gx MODULAR_HOME "$HOME/.modular"
 set -gx BUN_INSTALL "$HOME/.bun"
 
-set -gx PATH $PATH "/opt/homebrew/opt/openssl@3/bin" # Use openssl installed by homebrew
-set -gx PATH $PATH bin
-set -gx PATH $PATH "$HOME/bin"
-set -gx PATH $PATH "$HOME/.local/bin"
-set -gx PATH $PATH "$HOME/.progate/bin"
-set -gx PATH $PATH "$HOME/Library/Application Support/Coursier/bin"
-set -gx PATH $PATH "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
-set -gx PATH $PATH "$HOME/Library/Android/sdk/platform-tools"
-set -gx PATH $PATH "$HOME/.pub-cache/bin"
-set -gx PATH $PATH "$HOME/Developer/flutter/bin"
-set -gx PATH $PATH "$HOME/.composer/vendor/bin"
-set -gx PATH $PATH "$HOME/.deno/bin"
-set -gx PATH $PATH "$HOME/.cargo/bin"
-set -gx PATH $PATH "$HOME/.rye/shims"
-set -gx PATH $PATH "$PNPM_HOME"
-set -gx PATH $PATH "$VOLTA_HOME/bin"
-set -gx PATH $PATH "$GOPATH/bin"
-set -gx PATH $PATH "$BUN_INSTALL/bin"
-set -gx PATH $PATH /opt/homebrew/sbin
-set -gx PATH $PATH /opt/homebrew/bin
+set -gx PATH "/opt/homebrew/opt/openssl@3/bin" $PATH # Use openssl installed by homebrew
+set -gx PATH bin $PATH
+set -gx PATH "$HOME/bin" $PATH
+set -gx PATH "$HOME/.local/bin" $PATH
+set -gx PATH "$HOME/.progate/bin" $PATH
+set -gx PATH "$HOME/Library/Application Support/Coursier/bin" $PATH
+set -gx PATH "$HOME/Library/Application Support/JetBrains/Toolbox/scripts" $PATH
+set -gx PATH "$HOME/Library/Android/sdk/platform-tools" $PATH
+set -gx PATH "$HOME/.pub-cache/bin" $PATH
+set -gx PATH "$HOME/Developer/flutter/bin" $PATH
+set -gx PATH "$HOME/.composer/vendor/bin" $PATH
+set -gx PATH "$HOME/.deno/bin" $PATH
+set -gx PATH "$HOME/.cargo/bin" $PATH
+set -gx PATH "$HOME/.rye/shims" $PATH
+set -gx PATH "$PNPM_HOME" $PATH
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+set -gx PATH "$GOPATH/bin" $PATH
+set -gx PATH "$BUN_INSTALL/bin" $PATH
+set -gx PATH /opt/homebrew/sbin $PATH
+set -gx PATH /opt/homebrew/bin $PATH
 
 if test -f $HOME/Developer/google-cloud-sdk/path.fish.inc
     source $HOME/Developer/google-cloud-sdk/path.fish.inc
