39b247a93af1e14f56b62d29a281bfe82d70c21a
Kento Shirasawa <shirasawa.kento@gmail.com>
2025-01-17 10:21:40 +0900

    add(sh): slack cli path

--- .config/fish/path.fish
+++ .config/fish/path.fish
@@ -21,6 +21,7 @@ set -gx PATH bin $PATH
 set -gx PATH "$HOME/bin" $PATH
 set -gx PATH "$HOME/.local/bin" $PATH
 set -gx PATH "$HOME/.progate/bin" $PATH
+set -gx PATH "$HOME/.slack/bin" $PATH
 set -gx PATH "$HOME/Library/Application Support/Coursier/bin" $PATH
 set -gx PATH "$HOME/Library/Application Support/JetBrains/Toolbox/scripts" $PATH
 set -gx PATH "$HOME/Library/Android/sdk/platform-tools" $PATH
