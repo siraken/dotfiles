193330587cc943c605fbb44a9fd6b18feb7a9b71
siraken <shirasawa.kento@gmail.com>
2025-03-11 13:37:37 +0900

    fix(installer): use bun, add claude, remove unused

---
+++ installers/bun.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+packages=(
+  "typescript"
+  "nativescript"
+  "@ionic/cli"
+  "native-run"
+  "source-map-support"
+  "@aws-amplify/cli"
+  "firebase-tools"
+  "commitizen"
+  "@githubnext/github-copilot-cli"
+  "@angular/cli"
+  "@nestjs/cli"
+  "fastify-cli"
+  "yo"
+  "json-server"
+  "cordova"
+  "typeorm"
+  "wrangler"
+  "@lhci/cli"
+  "terminalizer"
+  "dredd"
+  "neovim"
+  "@anthropic-ai/claude-code"
+)
+
+# Install packages
+printf "Installing npm packages using pnpm..."
+
+for package in "${packages[@]}"; do
+  bun add -g $package
+done
