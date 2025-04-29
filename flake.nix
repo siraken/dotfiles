677e85ee4f0d69f4d9b9311920a96cb120383a8a
Kento Shirasawa <shirasawa.kento@gmail.com>
2025-04-29 23:18:19 +0900

    refactor

--- flake.nix
+++ flake.nix
@@ -1,10 +1,12 @@
 {
-  description = "Example nix-darwin system flake";
+  description = "siraken's Nix configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
