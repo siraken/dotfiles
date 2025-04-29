1e75ab6e64666e42c02db94b3f12265b783d1747
Kento Shirasawa <shirasawa.kento@gmail.com>
2025-04-30 02:08:10 +0900

    refactor: add configuration.nix

--- flake.nix
+++ flake.nix
@@ -7,39 +7,30 @@
       url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nixpkgs, nix-darwin, /* home-manager */ }:
   let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
+    darwinUser = "siraken";
+    darwinHost = "Kentos-MacBook-Pro";
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+    makeDarwinSystem = { hostname, username }: nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+        ./configuration.nix
+      ];
     };
   in
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Kentos-MacBook-Pro
-    darwinConfigurations."Kentos-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+    darwinConfigurations.${darwinHost} = makeDarwinSystem {
+      hostname = darwinHost;
+      username = darwinUser;
     };
   };
 }
