f5907b1b44a860e036428837f66a39842d3badd5
Kento Shirasawa <shirasawa.kento@gmail.com>
2025-05-23 23:20:10 +0900

    use double quotes, add claude config path

--- .config/bash/variable.sh
+++ .config/bash/variable.sh
@@ -3,10 +3,11 @@
 ##################################################
 export BASH_SILENCE_DEPRECATION_WARNING=1
 export CLICOLOR=true
-export TERM='xterm-256color'
-export EDITOR='nvim'
-export GH_URL='https://github.com'
-export AWS_PROFILE='default'
-export AWS_DEFAULT_REGION='ap-northeast-1'
+export TERM="xterm-256color"
+export EDITOR="nvim"
+export GH_URL="https://github.com"
+export AWS_PROFILE="default"
+export AWS_DEFAULT_REGION="ap-northeast-1"
+export CLAUDE_CONFIG_DARWIN="$HOME/Library/Application Support/Claude/claude_desktop_config.json"
 export DARWIN_USER=$(whoami)
 export DARWIN_HOST=$(hostname -s)
