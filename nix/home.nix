dad0c0ed84f7fc8cd0e244a42f4c1f69599bb7d5
Kento Shirasawa <shirasawa.kento@gmail.com>
2025-06-15 21:11:07 +0900

    add fish

--- nix/home.nix
+++ nix/home.nix
@@ -8,20 +8,10 @@
       ./environment/system-packages.nix
       # ./programs/bash.nix
       ./programs/zsh.nix
+      # ./programs/fish.nix
       # ./programs/direnv.nix
       # ./programs/starship.nix
     ];
-
-    programs = {
-      fish = {
-        enable = true;
-        vendor = {
-          completions.enable = true;
-          config.enable = true;
-          functions.enable = true;
-        };
-      };
-    };
 
     stateVersion = "25.05";
   };
---
+++ nix/programs/fish.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+{
+  programs.fish = {
+    enable = true;
+    vendor = {
+      completions.enable = true;
+      config.enable = true;
+      functions.enable = true;
+    };
+  };
+};
+
