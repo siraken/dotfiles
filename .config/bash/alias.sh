15ca297874b0510258fbb9201c1418b420307c36
Kento Shirasawa <shirasawa.kento@gmail.com>
2025-06-15 20:35:56 +0900

    alias: `cc` as `claude`

--- .config/bash/alias.sh
+++ .config/bash/alias.sh
@@ -29,6 +29,7 @@ alias rustrepl="evcxr"
 alias tf="terraform"
 alias spt="spotify_player"
 alias act="act --container-architecture linux/amd64"
+alias cc="claude"
 # Aliases for macOS
 alias ii="open"
 alias mamp-htdocs="cd /Applications/MAMP/htdocs"
