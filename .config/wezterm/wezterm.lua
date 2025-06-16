32c9aed01b49f8cfe8f6fe815a812e18ac5d50f5
Kento Shirasawa <shirasawa.kento@gmail.com>
2025-06-16 22:02:16 +0900

    add `use_ime`

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -31,6 +31,7 @@ config.audible_bell = 'SystemBeep'
 config.keys = keybinds.keys
 config.key_tables = keybinds.key_tables
 config.mouse_bindings = mouse_bindings
+config.use_ime = true
 config.default_prog = { "/bin/bash", "-l" }
 if is_mac then
   config.default_prog = { "/opt/homebrew/bin/bash", "-l" }
