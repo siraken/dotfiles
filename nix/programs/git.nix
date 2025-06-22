9f2364242dc68fa15b222b2598b0611598563bdd
Kento Shirasawa <shirasawa.kento@gmail.com>
2025-06-22 16:34:19 +0900

    Git設定ファイルをNixに移行し、不要な設定を削除

--- nix/programs/git.nix
+++ nix/programs/git.nix
@@ -2,10 +2,10 @@
 {
   programs.git = {
     enable = true;
-    
+
     userName = "Kento Shirasawa";
     userEmail = "shirasawa.kento@gmail.com";
-    
+
     extraConfig = {
       core = {
         autocrlf = "input";
@@ -13,39 +13,39 @@
         editor = "nvim";
         hooksPath = "~/.config/git/hooks";
       };
-      
+
       user = {
         username = "siraken";
         signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOjyytl+QL/ikAdL+f/xIl4/QeT/Pic9I+r/+nW7lAIL";
       };
-      
+
       github = {
         user = "siraken";
       };
-      
+
       init = {
         defaultBranch = "main";
       };
-      
+
       filter.lfs = {
         smudge = "git-lfs smudge -- %f";
         process = "git-lfs filter-process";
         required = true;
         clean = "git-lfs clean -- %f";
       };
-      
+
       diff = {
         tool = "difftastic";
       };
-      
+
       pager = {
         difftool = true;
       };
-      
+
       safe = {
         directory = "*";
       };
-      
+
       # OS-specific credential and GPG configuration
       credential = lib.mkMerge [
         {
@@ -61,8 +61,23 @@
           "https://gist.github.com".helper = "!gh auth git-credential";
         })
         # Windows support can be added when needed
+        # Windows:
+        # [credential "https://github.com"]
+        #   helper = !'C:\\Program Files\\GitHub CLI\\gh.exe' auth git-credential
+        # [credential "https://gist.github.com"]
+        #   helper = !'C:\\Program Files\\GitHub CLI\\gh.exe' auth git-credential
+        # [gpg]
+        #   format = ssh
+        #   # program = !'C:\\Program Files\\Git\\usr\\bin\\gpg.exe'
+        # [gpg "ssh"]
+        #   program = "C:/Users/siraken/AppData/Local/1Password/app/8/op-ssh-sign.exe"
+        # WSL:
+        # [gpg]
+        #   format = ssh
+        # [gpg "ssh"]
+        #   program = "/mnt/c/Users/siraken/AppData/Local/1Password/app/8/op-ssh-sign-wsl"
       ];
-      
+
       gpg = {
         format = "ssh";
       } // (lib.optionalAttrs pkgs.stdenv.isDarwin {
@@ -70,7 +85,7 @@
       }) // (lib.optionalAttrs pkgs.stdenv.isLinux {
         ssh.program = "/mnt/c/Users/siraken/AppData/Local/1Password/app/8/op-ssh-sign-wsl";
       });
-      
+
       color = {
         status = "auto";
         diff = "auto";
@@ -79,36 +94,36 @@
         grep = "auto";
         ui = "auto";
       };
-      
+
       commit = {
         verbose = true;
         gpgsign = true;
       };
-      
+
       push = {
         default = "simple";
         autoSetupRemote = true;
       };
-      
+
       pull = {
         rebase = true;
       };
-      
+
       fetch = {
         prune = true;
       };
-      
+
       rebase = {
         autostash = true;
       };
-      
+
       difftool = {
         prompt = false;
         sourcetree.cmd = "opendiff \"$LOCAL\" \"$REMOTE\"";
         difftastic.cmd = "difft \"$LOCAL\" \"$REMOTE\"";
         nvimdiff.cmd = "nvim -d \"$LOCAL\" \"$REMOTE\"";
       };
-      
+
       mergetool = {
         nvimdiff = {
           cmd = "nvim -d \"$LOCAL\" \"$REMOTE\" -ancestor \"$BASE\" -merge \"$MERGED\"";
@@ -119,12 +134,12 @@
           trustExitCode = true;
         };
       };
-      
+
       ghq = {
         root = "~/repos";
       };
     };
-    
+
     aliases = {
       d = "diff";
       b = "branch";
@@ -161,7 +176,7 @@
       cz = "!cz";
       update-from = "!f(){ echo \"Update from $1...\" && git checkout $1 && git pull && git checkout - && git merge -; };f";
     };
-    
+
     ignores = [
       ".DS_Store"
       "Thumbs.db"
