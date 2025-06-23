81c22facb41b1a385883311b2bf58e65ce2b20ce
Kento Shirasawa <shirasawa.kento@gmail.com>
2025-06-23 11:08:22 +0900

    fix: update git hooks configuration in home.nix

--- nix/home.nix
+++ nix/home.nix
@@ -45,7 +45,7 @@ in {
       ".config/husky".source = "${dotfilesPath}/.config/husky";
       ".config/mise".source = "${dotfilesPath}/.config/mise";
       ".config/ranger".source = "${dotfilesPath}/.config/ranger";
-      # ".config/git".source = "${dotfilesPath}/.config/git";
+      ".config/git/hooks".source = "${dotfilesPath}/.config/git/hooks";
       # ".config/emacs".source = "${dotfilesPath}/.config/emacs"; # Contains unsupported file types (sockets)
       ".config/nano".source = "${dotfilesPath}/.config/nano";
       ".config/nvim".source = "${dotfilesPath}/.config/nvim";
