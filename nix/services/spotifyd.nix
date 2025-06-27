1804a54f2ca8167fc0b08e58334216a7822b53c1
Kento Shirasawa <shirasawa.kento@gmail.com>
2025-06-27 16:43:35 +0900

    fix: update spotifyd settings for device and audio configuration

--- nix/services/spotifyd.nix
+++ nix/services/spotifyd.nix
@@ -7,8 +7,11 @@
       use_keyring = true;
       device_name = "spotifyd @ Kento's MacBook Pro";
       device_type = "computer";
+      device = "default";
       bitrate = 320;
+      backend = "portaudio";
       initial_volume = 100;
+      volume_controller = "softvol";
     };
   };
 }
