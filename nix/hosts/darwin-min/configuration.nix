b6241fdf76fcf0dee7eddf603bbff8e7bb04f230
Kento Shirasawa <shirasawa@novalumo.com>
2025-08-10 16:15:52 +0900

    add: `darwin-min`

---
+++ nix/hosts/darwin-min/configuration.nix
@@ -0,0 +1,102 @@
+# https://nix-darwin.github.io/nix-darwin/manual/index.html
+{ pkgs, modulePath, ... }:
+{
+  nix = {
+    # Necessary for using flakes on this system.
+    settings.experimental-features = "nix-command flakes";
+  };
+
+  networking = {
+    hostName = "Kentos-Darwin-Min";
+    localHostName = "Kentos-Darwin-Min";
+  };
+
+  imports = [
+    # ../../services/sketchybar.nix
+    ../../services/skhd.nix
+    ../../services/spotifyd.nix
+    ../../services/yabai.nix
+    # ../../programs/tmux.nix
+  ];
+
+  environment = {
+    systemPackages = [
+      pkgs.nil
+    ];
+  };
+
+  system = {
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    stateVersion = 5;
+    primaryUser = "siraken";
+    defaults = {
+      # TODO:
+      # defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -int 1
+      # defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
+      dock = {
+        orientation = "bottom";
+        tilesize = 40;
+        magnification = false;
+        largesize = 64;
+        autohide = true;
+        autohide-time-modifier = 0.5;
+        autohide-delay = 0.2;
+        show-recents = true;
+        mineffect = "scale";
+        static-only = false;
+        scroll-to-open = false;
+        launchanim = true;
+      };
+      finder = {
+        AppleShowAllExtensions = false;
+        AppleShowAllFiles = false;
+        ShowPathbar = true;
+        ShowStatusBar = true;
+      };
+      CustomUserPreferences = {
+        "com.microsoft.VSCode" = {
+          ApplePressAndHoldEnabled = false;
+        };
+      };
+    };
+  };
+
+  security = {
+    pam = {
+      services = {
+        sudo_local = {
+          watchIdAuth = true;
+        };
+      };
+    };
+  };
+
+  homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "none";
+    };
+    taps = [
+    ];
+    brews = [
+    ];
+    casks = [
+    ];
+    masApps = {
+    };
+  };
+
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.hack
+      source-code-pro
+      monocraft
+      udev-gothic
+    ];
+  };
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+}
---
+++ nix/hosts/darwin-min/home.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+let
+  username = "siraken";
+  dotfilesPath = "${config.home.homeDirectory}/dotfiles";
+in
+{
+  imports = [
+    # ../../environment/system-packages.nix
+    # ../../programs/bash.nix
+    ../../programs/git.nix
+    ../../programs/zsh.nix
+    ../../programs/zoxide.nix
+    # ../../programs/fish.nix
+    ../../programs/tmux.nix
+    ../../programs/direnv.nix
+    ../../programs/starship.nix
+  ];
+
+  home = {
+    stateVersion = "25.05";
+
+    file = import ../../modules/dotfiles.nix {
+      inherit config dotfilesPath;
+    } // {
+
+    };
+  };
+
+  programs.home-manager.enable = true;
+}
