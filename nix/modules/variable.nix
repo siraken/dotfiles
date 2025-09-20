81272a3fdda9b6c830afffad75428503eb9547d7
Kento Shirasawa <shirasawa@novalumo.com>
2025-09-20 22:25:10 +0900

    add variable.nix

---
+++ nix/modules/variable.nix
@@ -0,0 +1,9 @@
+{}: {
+  "XDG_CONFIG_HOME" = "$HOME/.config";
+  "SSH_AUTH_SOCK" = "$HOME/.1password/agent.sock";
+  "GOPATH" = "$HOME/go";
+  "MODULAR_HOME" = "$HOME/.modular";
+  "COMPOSER_HOME" = "$XDG_CONFIG_HOME/composer";
+  "BUN_INSTALL" = "$HOME/.bun";
+  "JAVA_HOME" = "/opt/homebrew/opt/openjdk@21/libexec/openjdk.jdk/Contents/Home";
+}
