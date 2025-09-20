8da518bdac257f396d9766d2b612e01b1b2728bc
Kento Shirasawa <shirasawa@novalumo.com>
2025-09-20 21:33:30 +0900

    adjust yazi config

--- nix/programs/yazi.nix
+++ nix/programs/yazi.nix
@@ -5,5 +5,16 @@
     enableBashIntegration = true;
     enableZshIntegration = true;
     enableFishIntegration = true;
+    settings = {
+      mgr = {
+        ratio = [2 3 3];
+        sort_by = "none";
+        sort_sensitive = false;
+        sort_reverse = false;
+        sort_dir_first = true;
+        show_symlink = true;
+        mouse_events = ["click" "scroll" "touch" "move" "drag"];
+      };
+    };
   };
 }
