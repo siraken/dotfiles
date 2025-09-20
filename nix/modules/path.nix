38c2ca7315dd8b55d161034000227023c5de9db4
Kento Shirasawa <shirasawa@novalumo.com>
2025-09-20 21:56:51 +0900

    add `path.nix`

---
+++ nix/modules/path.nix
@@ -0,0 +1,21 @@
+{}: [
+  "/opt/homebrew/opt/openssl@3/bin" # Use openssl installed by homebrew
+  "/opt/homebrew/sbin"
+  "/opt/homebrew/bin"
+  "bin"
+  "$HOME/bin"
+  "$HOME/.local/bin"
+  "$HOME/.progate/bin"
+  "$HOME/.slack/bin"
+  "$HOME/Library/Application Support/Coursier/bin"
+  "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
+  "$HOME/Library/Android/sdk/platform-tools"
+  "$HOME/.pub-cache/bin"
+  "$HOME/Developer/flutter/bin"
+  "$HOME/.deno/bin"
+  "$HOME/.cargo/bin"
+  "$HOME/.rye/shims"
+  "$COMPOSER_HOME/vendor/bin"
+  "$HOME/go/bin"
+  "$HOME/.bun/bin"
+]
