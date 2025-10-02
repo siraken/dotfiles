b137f69fae3454e749d4468004cf7cb827affeac
Kento Shirasawa <shirasawa@novalumo.com>
2025-10-02 14:23:20 +0900

    wez: define fonts

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -67,10 +67,10 @@ config.colors = {
 }
 config.color_scheme = "Tokyo Night"
 config.font = wezterm.font_with_fallback({
-  "JetBrains Mono",
-  "Hack Nerd Font Mono",
-  "Hiragino Sans",
-  "Noto Sans JP",
+  { family = "JetBrains Mono", weight = "regular" },
+  { family = "Hack Nerd Font Mono", weight = "regular" },
+  { family = "Hiragino Sans", weight = "regular" },
+  { family = "Noto Sans JP", weight = "regular" },
 })
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.adjust_window_size_when_changing_font_size = false
