5e7f76d1a94841482b0a4c774a5f05aab43435c6
Kento Shirasawa <shirasawa@novalumo.com>
2025-10-07 21:58:45 +0900

    fix(nix): enable bash

--- nix/programs/bash.nix
+++ nix/programs/bash.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   programs.bash = {
-    enable = false;
+    enable = true;
     enableCompletion = true;
     # historyControl = ["ignoredups" "erasedups"];
     # historyIgnore = ["ls *" "cd *" "pwd" "exit"];
