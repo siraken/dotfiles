425cb6522b16b8adb8cc2d956381e4a422f04399
Kento Shirasawa <shirasawa@novalumo.com>
2025-11-04 21:05:02 +0900

    match darwin-min and darwin

--- nix/hosts/darwin-min/home.nix
+++ nix/hosts/darwin-min/home.nix
@@ -11,20 +11,45 @@ let
 in
 {
   imports = [
+    inputs.op-shell-plugins.hmModules.default
     # ../../environment/system-packages.nix
+    # programs
+    ../../programs/1password-shell-plugins.nix
+    ../../programs/alacritty.nix
     # ../../programs/bash.nix
-    ../../programs/git.nix
-    ../../programs/zsh.nix
-    ../../programs/zoxide.nix
-    # ../../programs/fish.nix
-    ../../programs/tmux.nix
-    ../../programs/yazi.nix
+    ../../programs/bat.nix
+    ../../programs/bottom.nix
+    # ../../programs/difftastic.nix
     ../../programs/direnv.nix
+    ../../programs/fish.nix
+    ../../programs/ghostty.nix
+    ../../programs/git.nix
+    ../../programs/helix.nix
+    ../../programs/kakoune.nix
+    ../../programs/kitty.nix
+    ../../programs/lazydocker.nix
+    ../../programs/lazygit.nix
+    ../../programs/mise.nix
+    ../../programs/neovide.nix
     ../../programs/starship.nix
+    ../../programs/tmux.nix
+    ../../programs/vim.nix
+    # ../../programs/vscode.nix
+    ../../programs/yazi.nix
+    ../../programs/yt-dlp.nix
+    ../../programs/zed.nix
+    ../../programs/zoxide.nix
+    ../../programs/zsh.nix
+    # services
+    # ../../services/aerospace.nix
+    # ../../services/jankyborders.nix
   ];
 
   home = {
     stateVersion = "25.11";
+    # preferXdgDirectories = true; # to be enabled
+    # sessionVariables = import ../../modules/variable.nix;
+    sessionPath = import ../../modules/path.nix { };
 
     file =
       import ../../modules/symlinks.nix {
@@ -33,6 +58,22 @@ in
       // {
 
       };
+
+    shell = {
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      enableFishIntegration = true;
+      enableShellIntegration = true;
+    };
+
+    activation = import ../../modules/home-activation.nix {
+      inherit
+        config
+        pkgs
+        lib
+        dotfilesPath
+        ;
+    };
   };
 
   programs.home-manager.enable = true;
