6546d03836f03a4e18fe8eb4ff54b14d09bd7e6d
Kento Shirasawa <shirasawa@novalumo.com>
2025-11-20 22:18:51 +0900

    remove `awsmfa` function

--- .config/bash/function.sh
+++ .config/bash/function.sh
@@ -85,36 +85,3 @@ function cccw() {
   wezterm cli activate-pane --pane-id $RIGHT_TOP
   wezterm cli split-pane --bottom --percent 50
 }
-
-function awsmfa() {
-  if [[ -z "$1" ]]; then
-    echo "エラー: OTPトークンコードを引数として渡してください。" >&2
-    return 1
-  fi
-
-  local OTP=$1
-  local MFA_DEVICE_ARN
-  local TOKEN
-  local AWS_ACCESS_KEY_ID
-  local AWS_SECRET_ACCESS_KEY
-  local AWS_SESSION_TOKEN
-
-  MFA_DEVICE_ARN=$(aws iam list-mfa-devices | jq '.MFADevices[0].SerialNumber' --raw-output)
-  TOKEN=$(aws sts get-session-token --serial-number $MFA_DEVICE_ARN --token-code $OTP 2> /dev/null)
-
-  if [[ -z "$TOKEN" ]]; then
-    echo "エラー: トークンの取得に失敗しました。OTPが間違っているか、期限切れの可能性があります。" >&2
-    return 1
-  fi
-
-  AWS_ACCESS_KEY_ID=$(echo $TOKEN | jq '.Credentials.AccessKeyId' --raw-output)
-  AWS_SECRET_ACCESS_KEY=$(echo $TOKEN | jq '.Credentials.SecretAccessKey' --raw-output)
-  AWS_SESSION_TOKEN=$(echo $TOKEN | jq '.Credentials.SessionToken' --raw-output)
-
-  export AWS_ACCESS_KEY_ID
-  export AWS_SECRET_ACCESS_KEY
-  export AWS_SESSION_TOKEN
-
-  echo "一時的な認証情報を設定しました。"
-  echo "Expires at: $(echo $TOKEN | jq '.Credentials.Expiration' --raw-output)"
-}
