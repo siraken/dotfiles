83c0ccd079c615168fd756cf873bc7e6dd5f11e0
Kento Shirasawa <shirasawa@novalumo.com>
2025-12-23 00:46:37 +0900

    merge into coding-agents.nix

--- nix/programs/codex.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }:
-{
-  programs.codex = {
-    enable = true;
-  };
-}
--- nix/programs/coding-agents.nix
+++ nix/programs/coding-agents.nix
@@ -3,4 +3,13 @@
   programs.claude-code = {
     enable = true;
   };
+
+  programs.codex = {
+    enable = true;
+  };
+
+  programs.gemini-cli = {
+    enable = true;
+    commands = { };
+  };
 }
