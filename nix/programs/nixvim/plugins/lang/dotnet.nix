dbb75100af86918174b9dac2093a884c2be57aec
Kento Shirasawa <shirasawa.kento@gmail.com>
2025-12-26 21:45:52 +0900

    Revert "feat(nixvim): enable language-specific plugins (#42)"
    This reverts commit 7df74abf81cf85751cedb49ed78228552ef66618.

--- nix/programs/nixvim/plugins/lang/dotnet.nix
+++ nix/programs/nixvim/plugins/lang/dotnet.nix
@@ -1,19 +1,22 @@
 # .NET/C# development
-{ pkgs, ... }:
-let
-  omnisharp-extended-lsp = pkgs.vimUtils.buildVimPlugin {
-    pname = "omnisharp-extended-lsp-nvim";
-    version = "2024-12-26";
-    src = pkgs.fetchFromGitHub {
-      owner = "Hoffs";
-      repo = "omnisharp-extended-lsp.nvim";
-      rev = "a47388e5417e7f1cfa6962cc441a23c4c5fb2151";
-      hash = "sha256-0cRngH9BFuBbEu7007Xqr5zVJSBUowni7jxaMxGwnzU=";
-    };
-  };
-in
+{ ... }:
+# TODO: Re-enable when hash is updated
+# { pkgs, ... }:
+# let
+#   omnisharp-extended-lsp = pkgs.vimUtils.buildVimPlugin {
+#     pname = "omnisharp-extended-lsp-nvim";
+#     version = "2024-12-26";
+#     src = pkgs.fetchFromGitHub {
+#       owner = "Hoffs";
+#       repo = "omnisharp-extended-lsp.nvim";
+#       rev = "5b667daab948090931de0b31e4d58c6a6b0dce2f";
+#       hash = "sha256-YBJkW4ycKSWdB3lqZJKOCaieUt7xWyYw5kqN8RZxn90=";
+#     };
+#   };
+# in
 {
   programs.nixvim = {
-    extraPlugins = [ omnisharp-extended-lsp ];
+    # TODO: Re-enable omnisharp-extended-lsp when hash is updated
+    # extraPlugins = [ omnisharp-extended-lsp ];
   };
 }
