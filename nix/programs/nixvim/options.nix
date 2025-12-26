545b60947421529805c9f1b62ceedecb61ac8b86
Kento Shirasawa <shirasawa@novalumo.com>
2025-12-26 21:47:01 +0900

    Revert "feat(nixvim): add Neovide GUI settings" (#48)

--- nix/programs/nixvim/options.nix
+++ nix/programs/nixvim/options.nix
@@ -76,18 +76,6 @@
     extraConfigLua = ''
       vim.scriptencoding = "utf-8"
       vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
-
-      -- Neovide GUI settings
-      if vim.g.neovide then
-        vim.g.neovide_transparency = 0.5
-        vim.g.transparency = 0.8
-        vim.g.neovide_background_color = "#0f1117" .. string.format("%x", math.floor(255 * 0.8))
-        vim.g.neovide_window_blurred = true
-        vim.g.neovide_floating_shadow = true
-        vim.g.neovide_floating_shadow_amount_x = 2.0
-        vim.g.neovide_floating_shadow_amount_y = 2.0
-        vim.g.neovide_light_radius = 5
-      end
     '';
   };
 }
