b71eed4fbc183ac85cbe225a90d668411507f64e
Kento Shirasawa <shirasawa@novalumo.com>
2026-01-01 17:31:05 +0900

    start nvim from minimal

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,22 +1,32 @@
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system({
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazy_repo = "https://github.com/folke/lazy.nvim.git"
+if not (vim.uv or vim.loop).fs_stat(lazy_path) then
+  local out = vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
-    lazypath,
+    lazy_repo,
+    lazy_path,
   })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-vim.opt.rtp:prepend(lazypath)
 
--- Setup leader key before lazy
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.opt.rtp:prepend(lazy_path)
 
 require("lazy").setup({
+  spec = {
+    { import = "plugins.core" },
+  },
   defaults = {
     lazy = false,
     version = false,
