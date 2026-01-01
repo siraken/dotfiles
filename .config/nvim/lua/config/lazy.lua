5545c91451a2b16ad7cc524cdfcd3f60473c0f20
Kento Shirasawa <shirasawa@novalumo.com>
2026-01-01 18:22:12 +0900

    feat(nvim): add new plugins to lazy-lock.json and enable LSP plugin import in lazy.lua

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -34,7 +34,7 @@ require("lazy").setup({
     -- { import = "plugins.editor" },
     { import = "plugins.git" },
     { import = "plugins.lang" },
-    -- { import = "plugins.lsp" },
+    { import = "plugins.lsp" },
     -- { import = "plugins.misc" },
     -- { import = "plugins.treesitter" },
     -- { import = "plugins.ui" },
