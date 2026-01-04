5ecbeb895a13560a022db786d3d065e209c3b523
Kento Shirasawa <shirasawa@novalumo.com>
2026-01-04 15:58:37 +0900

    fix(emacs): format extraPackages for improved readability

--- nix/programs/emacs.nix
+++ nix/programs/emacs.nix
@@ -3,50 +3,51 @@
   programs.emacs = {
     enable = true;
 
-    extraPackages = epkgs: with epkgs; [
-      use-package
-      auto-complete
-      helm
-      flycheck
-      projectile
-      cider
-      tide
-      slime
-      paredit
-      dashboard
-      neotree
-      undo-tree
-      rainbow-delimiters
-      which-key
-      multiple-cursors
-      evil
-      # Mode
-      lsp-mode
-      web-mode
-      emmet-mode
-      js2-mode
-      markdown-mode
-      clojure-mode
-      php-mode
-      yaml-mode
-      scss-mode
-      dockerfile-mode
-      csharp-mode
-      rust-mode
-      swift-mode
-      lua-mode
-      kotlin-mode
-      nim-mode
-      go-mode
-      # Git
-      magit
-      git-gutter
-      # Line
-      doom-modeline
-      # Theme
-      zenburn-theme
-      doom-themes
-    ];
+    extraPackages =
+      epkgs: with epkgs; [
+        use-package
+        auto-complete
+        helm
+        flycheck
+        projectile
+        cider
+        tide
+        slime
+        paredit
+        dashboard
+        neotree
+        undo-tree
+        rainbow-delimiters
+        which-key
+        multiple-cursors
+        evil
+        # Mode
+        lsp-mode
+        web-mode
+        emmet-mode
+        js2-mode
+        markdown-mode
+        clojure-mode
+        php-mode
+        yaml-mode
+        scss-mode
+        dockerfile-mode
+        csharp-mode
+        rust-mode
+        swift-mode
+        lua-mode
+        kotlin-mode
+        nim-mode
+        go-mode
+        # Git
+        magit
+        git-gutter
+        # Line
+        doom-modeline
+        # Theme
+        zenburn-theme
+        doom-themes
+      ];
 
     extraConfig = ''
       ;;; init.el --- Initialization file for Emacs
