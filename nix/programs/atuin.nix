e3fe202400fd7c314f7485a4bf931d080a3ecb98
Kento Shirasawa <shirasawa@novalumo.com>
2026-01-08 00:35:34 +0900

    feat(atuin): add settings configuration for Atuin program

--- nix/programs/atuin.nix
+++ nix/programs/atuin.nix
@@ -5,9 +5,23 @@
     enableBashIntegration = true;
     enableFishIntegration = true;
     enableZshIntegration = true;
+    settings = {
+      dialect = "us";
+      auto_sync = true;
+      search_mode = "fuzzy";
+      style = "full";
+      invert = true;
+      show_preview = true;
+      exit_mode = "return-query"; # return-original or return-query
+      secrets_filter = true;
+      theme = {
+        name = "autumn";
+        debug = false;
+        max_depth = 10;
+      };
+    };
     # daemon = {
     #   enable = true;
     # };
-    # settings = { };
   };
 }
