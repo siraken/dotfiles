1434fcd2d23cd2023f4593d1e4a5533428cf016c
Kento Shirasawa <shirasawa@novalumo.com>
2026-01-07 21:01:48 +0900

    feat(kakoune): update configuration to enable autoComplete and autoReload with relative line numbers

--- nix/programs/kakoune.nix
+++ nix/programs/kakoune.nix
@@ -2,11 +2,13 @@
 {
   programs.kakoune = {
     enable = true;
-
     config = {
       colorScheme = "solarized-dark";
+      autoComplete = "prompt";
+      autoReload = "yes";
       numberLines = {
         enable = true;
+        relative = true;
         highlightCursor = true;
       };
     };
