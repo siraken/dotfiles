e4adf9172ef8ccf04886d3ea070549092bb43d86
Kento Shirasawa <shirasawa@novalumo.com>
2026-02-02 16:05:04 +0900

    chore: remove unused scripts

--- installers/claude.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-claude mcp add mcp-jetbrains -- bunx -y @jetbrains/mcp-proxy
-
-claude mcp list
--- installers/dart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pkgs=(
-  "dart_frog_cli"
-)
-
-for pkg in "${pkgs[@]}"; do
-  dart pub global activate $pkg
-done
