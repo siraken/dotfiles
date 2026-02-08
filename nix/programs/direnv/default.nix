7d220e9261f131858c298fda64a21fdeec2808d9
Kento Shirasawa <shirasawa@novalumo.com>
2026-02-08 15:40:44 +0900

    fix(direnv): adjust config

--- nix/programs/direnv/default.nix
+++ nix/programs/direnv/default.nix
@@ -5,6 +5,15 @@
     enableBashIntegration = true;
     enableFishIntegration = true;
     enableZshIntegration = true;
+    silent = false;
     nix-direnv.enable = true;
+    config = {
+      # load_dotenv = true;
+      strict_env = true;
+      warn_timeout = "10s";
+    };
+    mise = {
+      enable = false;
+    };
   };
 }
