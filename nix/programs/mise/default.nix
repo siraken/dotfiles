08dd884d581f615f076bc06118cdf32c9c016ad0
Kento Shirasawa <shirasawa@novalumo.com>
2026-02-11 01:06:02 +0900

    chore: tidy mise

--- nix/programs/mise/default.nix
+++ nix/programs/mise/default.nix
@@ -4,36 +4,6 @@ let
   prefixTools =
     prefix: tools: lib.mapAttrs' (name: value: lib.nameValuePair "${prefix}:${name}" value) tools;
 
-  # npm パッケージ
-  npm = {
-    # nativescript = "latest";
-    # "@ionic/cli" = "latest";
-    # native-run = "latest";
-    # source-map-support = "latest";
-    # "@aws-amplify/cli" = "latest";
-    # firebase-tools = "latest";
-    # commitizen = "latest";
-    # "@angular/cli" = "latest";
-    # fastify-cli = "latest";
-    yo = "latest";
-    # json-server = "latest";
-    # typeorm = "latest";
-    # terminalizer = "latest";
-    # dredd = "latest";
-    # neovim = "latest";
-    # web-push = "latest";
-    spago = "latest";
-    ccmanager = "latest";
-    dev3000 = "latest";
-    "@google/clasp" = "latest";
-    "@github/copilot" = "latest";
-    "@lhci/cli" = "latest";
-    "@nestjs/cli" = "latest";
-    cordova = "latest";
-    cordova-res = "latest";
-    md-to-pdf = "latest";
-  };
-
   # cargo パッケージ
   cargo = {
     # cargo-binstall = "latest";
@@ -57,20 +27,6 @@ let
     htmlbeautifier = "latest";
     # neovim = "latest";
   };
-
-  # pipx パッケージ
-  pipx = {
-    # watchdog = "latest";
-  };
-
-  # go パッケージ
-  go = {
-    # "golang.org/x/tools/cmd/godoc" = "latest"; # godoc
-    # "github.com/sheepla/pingu" = "latest"; # ping alternative: pingu
-    # "github.com/fatih/gomodifytags" = "latest"; # Go tool to modify struct field tags
-    # "github.com/antonmedv/llama" = "latest"; # Terminal file manager
-    # "github.com/maaslalani/nap" = "latest"; # Code snippets in terminal
-  };
 in
 {
   programs.mise = {
@@ -81,32 +37,15 @@ in
     globalConfig = {
       tools = {
         usage = "latest";
-        node = "latest";
         dotnet = "latest";
-        erlang = "latest";
-        elixir = "latest";
         ruby = "latest";
         zig = "latest";
-        uv = "latest";
-        rust = "latest";
       }
-      // prefixTools "npm" npm
       // prefixTools "cargo" cargo
-      // prefixTools "gem" gem
-      // prefixTools "pipx" pipx
-      // prefixTools "go" go;
+      // prefixTools "gem" gem;
 
       settings = {
         idiomatic_version_file_enable_tools = [ ];
-
-        npm = {
-          bun = false;
-        };
-
-        pipx = {
-          uvx = true;
-        };
-
         status = {
           missing_tools = "if_other_versions_installed";
           show_env = false;
