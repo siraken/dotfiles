983a9d09e7d193d65d7ed45feebbf79d0470dc3e
Kento Shirasawa <shirasawa@novalumo.com>
2026-02-20 10:17:38 +0900

    fix(border): set width to gap size

--- nix/services/darwin/jankyborders.nix
+++ nix/services/darwin/jankyborders.nix
@@ -5,7 +5,7 @@
     settings = {
       style = "round";
       hidpi = "off";
-      width = 8.0;
+      width = 6.0;
       active_color = "0xffffffff";
       inactive_color = "0x00000000";
       background_color = "0x00000000";
